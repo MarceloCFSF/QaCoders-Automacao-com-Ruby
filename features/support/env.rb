384e85d71e03d45d4258bb9d3ac304fcaffcfa43
MarceloCFSF <marcelo.cfsf@gmail.com>
2021-10-15 13:12:46 -0300

    Config cucumber.yml

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,10 +5,15 @@ require 'pry'
 
 require_relative 'helper.rb'
 
+ENVIRONMENT = ENV['ENVIRONMENT']
+puts "Ambiente >> #{ENVIRONMENT}"
+
+CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environments/#{ENVIRONMENT}.yml")
+
 world(Helper)
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
-  config.app_host = CONFIG('https://qacoders-dev-br.umbler.net/')
+  config.app_host = CONFIG['url_default']
   config.default_max_wait_time = 10
 end
