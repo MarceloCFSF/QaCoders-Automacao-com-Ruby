e1940397598202a33e9bb2e6e92bcaa3ba05898a
MarceloCFSF <marcelo.cfsf@gmail.com>
2021-10-18 13:02:48 -0300

    fix: Screenshots Format

--- features/support/helper.rb
+++ features/support/helper.rb
@@ -1,7 +1,7 @@
 module Helper
   def print_screen(file_name, result)
     file_path = "report/screenshots/tests_#{result}"
-    screenshot = "#{file_path}/#{file_name}"
+    screenshot = "#{file_path}/#{file_name}.png"
     page.save_screenshot(screenshot)
     attach(screenshot, 'image/png')
   end
